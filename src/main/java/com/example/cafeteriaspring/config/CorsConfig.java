8ad1739165737644a068f01259d8c7d53ddf132e
YARDY <u22239465@utp.edu.pe>
2024-10-01 17:11:30 -0500

    v1.1

--- src/main/java/com/example/cafeteriaspring/config/CorsConfig.java
+++ src/main/java/com/example/cafeteriaspring/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*");//vom
             }
         };
     }
